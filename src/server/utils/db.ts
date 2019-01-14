27b49d5dc236df8ad413e821c47e99b05b72e2a1
Emanuele Di Saverio <Emanuele.di.Saverio@frogdesign.com>
2019-01-14 15:23:02 +0100

    fix the tsc for libs

--- src/server/utils/db.ts
+++ src/server/utils/db.ts
@@ -1,45 +1,45 @@
-import env from 'constants/env'
-import sequelize, { Sequelize } from 'sequelize'
-import logger from 'utils/logger'
+// import env from 'constants/env'
+// import sequelize, { Sequelize } from 'sequelize'
+// import logger from 'utils/logger'
 
-let connection: Sequelize
+// let connection: Sequelize
 
-export const initDBConnection = async () => {
-  if (process.env.NODE_ENV === 'test') {
-    return Promise.resolve()
-  }
+// export const initDBConnection = async () => {
+//   if (process.env.NODE_ENV === 'test') {
+//     return Promise.resolve()
+//   }
 
-  if (connection) {
-    return Promise.resolve(connection)
-  }
+//   if (connection) {
+//     return Promise.resolve(connection)
+//   }
 
-  const dbConnectionString = `postgres://${env.MYAXA_DATABASE_USERNAME}:${
-    env.MYAXA_DATABASE_PASSWORD
-    }@${env.MYAXA_DATABASE_HOST}:${env.MYAXA_DATABASE_PORT}/${
-    env.MYAXA_DATABASE_NAME
-    }`
+//   const dbConnectionString = `postgres://${env.MYAXA_DATABASE_USERNAME}:${
+//     env.MYAXA_DATABASE_PASSWORD
+//     }@${env.MYAXA_DATABASE_HOST}:${env.MYAXA_DATABASE_PORT}/${
+//     env.MYAXA_DATABASE_NAME
+//     }`
 
-  const dialect = env.MYAXA_DATABASE_DIALECT
-  const operatorsAliases = sequelize.Op
+//   const dialect = env.MYAXA_DATABASE_DIALECT
+//   const operatorsAliases = sequelize.Op
 
-  const { Sequelize } = sequelize
-  const instance = new Sequelize(dbConnectionString, {
-    dialect,
-    operatorsAliases,
-    logging: false
-  })
+//   const { Sequelize } = sequelize
+//   const instance = new Sequelize(dbConnectionString, {
+//     dialect,
+//     operatorsAliases,
+//     logging: false
+//   })
 
-  try {
-    await instance.authenticate()
-    logger.info(
-      `Connected succesfully to the database with dialect ${dialect}`
-    )
-    connection = instance
-    return Promise.resolve(connection)
-  } catch (err) {
-    logger.error(err)
-    return Promise.reject()
-  }
-}
+//   try {
+//     await instance.authenticate()
+//     logger.info(
+//       `Connected succesfully to the database with dialect ${dialect}`
+//     )
+//     connection = instance
+//     return Promise.resolve(connection)
+//   } catch (err) {
+//     logger.error(err)
+//     return Promise.reject()
+//   }
+// }
 
-export const dbConnection = () => connection
+// export const dbConnection = () => connection
